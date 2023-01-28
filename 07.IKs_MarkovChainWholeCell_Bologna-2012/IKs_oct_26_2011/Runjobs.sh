85dc68b781a3fdfc81506a8d54c6fe296c90a723
Sanjay R. Kharche, Ph.D <srk.london@yahoo.com>
2023-01-28 13:34:58 -0500

    Delete 07.IKs_MarkovChainWholeCell_Bologna-2012 directory

--- 07.IKs_MarkovChainWholeCell_Bologna-2012/IKs_oct_26_2011/Runjobs.sh
+++
@@ -1,6 +0,0 @@
-cd /home/sanjay/Bologna/IKs_oct_26_2011/acuteEPA
-nohup matlab -nodisplay -nojvm -nodesktop < drive.m > matlablog.txt &
-cd /home/sanjay/Bologna/IKs_oct_26_2011/control
-nohup matlab -nodisplay -nojvm -nodesktop < drive.m > matlablog.txt &
-cd ..
-
